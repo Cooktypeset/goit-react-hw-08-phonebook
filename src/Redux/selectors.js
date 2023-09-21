00e0b7ee742f819172b48459dedf908b16231c67
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-21 22:51:48 +0300

    updSelect

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -1,12 +1,22 @@
 import { createSelector } from '@reduxjs/toolkit';
 
+//////// Contacts-State
 export const selectContacts = state => state.contacts.items;
-export const selectFilterValue = state => state.filter;
 export const selectLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
 
+//////// Filter-State
+export const selectFilterValue = state => state.filter;
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilterValue],
   (items, filter) =>
     items.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()))
 );
+
+//////// Auth-State
+
+
+export const selectUser = state => state.auth.user;
+export const selectToken = state => state.auth.token;
+export const selectIsLogged = state => state.auth.isLoggedIn;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
