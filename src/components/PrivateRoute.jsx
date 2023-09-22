8c6c189b14ed7961fc90d37199f1c7579fd04eb5
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-22 09:24:08 +0300

    newCreatPrRoute

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { selectIsLogged, selectIsRefreshing } from 'Redux/selectors';
+import { Navigate } from 'react-router-dom';
+
+export const PrivateRoute = component => {
+  const isLoggedIn = useSelector(selectIsLogged);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+
+  return shouldRedirect ? <Navigate to="/" /> : component.component;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.node,
+};
