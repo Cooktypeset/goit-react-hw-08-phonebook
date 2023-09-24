6b32f819035c88ba3cd0c46890110fee2f1e9f6f
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-24 13:13:13 +0300

    updCssCreatJsxWelcom

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectIsLogged, selectUser } from 'Redux/selectors';
+import { Container } from 'components/container/Container';
+import css from './WelcomePage.module.css';
+
+const WelcomePage = () => {
+    const isLogged = useSelector(selectIsLogged);
+    const userName = useSelector(selectUser);
+    return (
+        <Container>
+            <div className={css.welcome_wrapper}>
+                <h1>Welcome</h1>
+                <>
+                    <p className={css.text__style}>{userName.name}</p>
+                    <p className={css.text__style}>You already is Logged In</p>
+                </>
+
+                <>
+                    <p className={css.text__style}>There is your privet PhoneBook</p>
+                    <p className={css.text__style}>Please Login or Register</p>
+                </>
+            </div>
+        </Container>
+    );
+};
+
+export default WelcomePage;
