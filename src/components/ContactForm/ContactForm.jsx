e19c8fd7b0b1e44c8bea56bcdcd75a02fd69b18b
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-24 23:37:42 +0300

    updContForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,9 +8,9 @@ import css from "./ContactForm.module.css";
 export const ContactForm = () => {
 
   const dispatch = useDispatch();
-  const contacts = useSelector( selectContacts);
+  const contacts = useSelector(selectContacts);
   
- const handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const newContact = {};
 
@@ -32,48 +32,50 @@ export const ContactForm = () => {
       event.currentTarget.reset();
       return;
     }
-   dispatch(addContacts({
-     name: newContact.name,
-     phone: newContact.number
-   }));
+    dispatch(addContacts({
+      name: newContact.name,
+      phone: newContact.number
+    }));
     event.currentTarget.reset();
   };
 
-    return(
-     <form  className={css.form}  onSubmit={handleSubmit}>
-        <label className={css.subTitle}>
-          Name
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-ЯІіЇїҐґ' \-\u0400-\u04FF]+$"
-            title="Name may contain only letters, apostrophe, dash and spaces. 
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.subTitle}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-ЯІіЇїҐґ' \-\u0400-\u04FF]+$"
+          title="Name may contain only letters, apostrophe, dash and spaces. 
           For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            placeholder="Enter name"
-            // value={name}
-            // onChange={handleChange}
-          />
-        </label>
+          placeholder="Enter name"
+        // value={name}
+        // onChange={handleChange}
+        />
+      </label>
 
-        <label className={css.subTitle}>
-          Number
-          <input
-            className={css.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            placeholder="Enter your number"
-            // value={number}
-                // onChange={handleChange}
-             required
-          />
-        </label>
+      <label className={css.subTitle}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          placeholder="Enter your number"
+          // value={number}
+          // onChange={handleChange}
+          required
+        />
+      </label>
 
-        <button type="submit" className={css.btn}>
-          Add contact
-        </button>
-      </form>
-    );
-}
+      <button type="submit" className={css.btn}>
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
