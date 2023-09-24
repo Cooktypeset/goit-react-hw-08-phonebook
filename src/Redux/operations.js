47ebe7bbc8e5c3dc796f00deb2d758b3e950e767
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-24 13:52:05 +0300

    refOperations

--- src/Redux/operations.js
+++ src/Redux/operations.js
@@ -1,31 +1,122 @@
+import Notiflix from 'notiflix';
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const API_URL = "https://6507fbcc56db83a34d9b8a97.mockapi.io"
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-    const response = await axios.get(
-        `${API_URL}/contacts`
-    );
-    return response.data;
-});
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
-export const deleteContacts = createAsyncThunk(
-    'contacts/deleteContact', async id => {
-        const response = await axios.delete(
-            `${API_URL}/contacts/${id}`
-        );
-        return response.data;
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
+  }
 );
 
 export const addContacts = createAsyncThunk(
-    'contacts/addContacts',
-    async contact => {
-        const response = await axios.post(
-            `${API_URL}/contacts`,
-            contact
-        );
-        return response.data;
+  'contacts/addContacts',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contact);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
+  }
+);
+
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateContacts = createAsyncThunk(
+  'contacts/updateContacts',
+  async ([id, data], thunkAPI) => {
+    try {
+      const response = await axios.patch(`/contacts/${id}`, data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/signup', contact);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      Notiflix.Notify.warning(
+        `Something wrong. ${error.message}. Maybe this eMail already used`
+      );
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logInUser = createAsyncThunk(
+  'auth/logInUser',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/login', contact);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOutUser = createAsyncThunk(
+  'auth/logOutUser',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post('/users/logout');
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getCurrentUser = createAsyncThunk(
+  'auth/getCurrentUser',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
