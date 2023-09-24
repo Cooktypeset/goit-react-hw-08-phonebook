2519573131ba5ea27b95fdd7a50d5b7d35d4772a
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-24 12:30:54 +0300

    fixPageMain

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react';
 import { Header } from 'components/header/Header';
+import { Outlet } from 'react-router-dom';
 
 export const Main = () => {
     return (
         <div>
             <Header />
-            <Suspense
-            ></Suspense>
+            <Suspense>
+             <Outlet/>
+            </Suspense>
         </div>
     );
 };
